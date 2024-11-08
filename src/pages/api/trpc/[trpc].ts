811db4ae3894b74b6afc4ee3921f36e9ca18ed64
KATT <alexander@n1s.se>
2024-11-08 19:01:47 +0100

    where are the events

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,6 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/routers/_app";
+import * as next from 'next'
 
 export async function createContext({
   req,
@@ -10,10 +11,36 @@ export async function createContext({
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
 
-export default trpcNext.createNextApiHandler({
+const trpcHandler = trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
   onError: (opts) => {
     console.error('onError handler', opts.error)
   }
 });
+const handler: next.NextApiHandler = (req, res) => {
+
+  console.log('request')
+  req.once('close', () => {
+    console.log('req closed')
+  })
+
+  res.once('close', () => {
+    console.log('res closed');
+  })
+  req.socket.once('close', () => {
+    console.log('socket closed')
+  })
+  req.socket.once('end', () => {
+    console.log('socket end');
+  })
+  req.once('aborted', () => {
+    console.log('req abort');
+  })
+  req.once('pause', () => {
+    console.log('---------pause')
+  })
+  return trpcHandler(req,res)
+}
+
+export default handler;
